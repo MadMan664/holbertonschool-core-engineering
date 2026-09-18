c1bfb70eb7805e8758f63de55ad11189a8a8c40d
MadMan664 <madhi704@gmail.com>
2026-09-18 18:06:37 +0300

    added structured_output.py file

---
+++ python_fundamentals/structured_output.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3  
+import math
+
+language = "Python"
+version = 3
+Pi_approx = math.pi
+computation_valid = (5==5)
+
+print(f"Language: {language}")
+print(f"Version: {version}")
+print(f"Pi approx: {Pi_approx:.2f}")
+print(f"Computation valid: {computation_valid}")

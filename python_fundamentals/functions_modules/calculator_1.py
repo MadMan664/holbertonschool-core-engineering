6c9455ec5cac27ca8a9163ffaed60e046e1e397f
MadMan664 <madhi704@gmail.com>
2026-09-19 20:55:06 +0300

    functions_modules: use official calculator_1.py (int-truncating div)

--- python_fundamentals/functions_modules/calculator_1.py
+++ python_fundamentals/functions_modules/calculator_1.py
@@ -1,22 +1,51 @@
 #!/usr/bin/env python3
-"""Provide basic arithmetic functions for import."""
-
-
 def add(a, b):
-    """Return a + b."""
-    return a + b
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
 
 
 def sub(a, b):
-    """Return a - b."""
-    return a - b
+    """My subtraction function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a - b
+    """
+    return (a - b)
 
 
 def mul(a, b):
-    """Return a * b."""
-    return a * b
+    """My multiplication function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a * b
+    """
+    return (a * b)
 
 
 def div(a, b):
-    """Return a / b."""
-    return a / b
+    """My division function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a / b
+    """
+    return int(a / b)

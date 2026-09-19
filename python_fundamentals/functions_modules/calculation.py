033f516c1f917acdba27334e4c34c7cb3ec808dc
MadMan664 <madhi704@gmail.com>
2026-09-19 20:44:36 +0300

    functions_modules: display division result as int when whole

--- python_fundamentals/functions_modules/calculation.py
+++ python_fundamentals/functions_modules/calculation.py
@@ -5,7 +5,10 @@ from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
     a = 10
     b = 5
+    result = div(a, b)
+    if result == int(result):
+        result = int(result)
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b)))
+    print("{} / {} = {}".format(a, b, result))
